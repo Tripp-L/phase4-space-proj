c600bc12ba898ac689315c7a3ecf9cc1cba18678
trippl <tlaursen8@yahoo.com>
2024-06-17 09:58:46 -0400

    Destinations and Missions complete

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,19 +5,15 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function MyNavbar() {
-
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/">Interstellar Explorer</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/dashboard">Dashboard</Nav.Link>
             <Nav.Link as={Link} to="/missions">Missions</Nav.Link>
             <Nav.Link as={Link} to="/spacecrafts">Spacecrafts</Nav.Link>
-            <Nav.Link as={Link} to="/explore">Explore</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom/client';
 import App from "./components/App";
 import "./index.css";
 import { BrowserRouter } from 'react-router-dom';
 
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <BrowserRouter> 
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
+</React.StrictMode>
 );
