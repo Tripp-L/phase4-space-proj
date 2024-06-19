3ba3126660fd97d89c4b8ccf3aa77562cfcf5cc0
George Manuell Irizarry <mojomojoke9@gmail.com>
2024-06-19 19:27:32 -0400

    added nullable false to password hash under class players

--- server/config.py
+++ server/config.py
@@ -1,7 +1,28 @@
-import os
 
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'supersecretjwtkey')
+from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+migrate = Migrate(app, db)
+db.init_app(app)
+
+
+api = Api(app)
+
+
+CORS(app)
